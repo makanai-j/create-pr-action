b5f48c519dcdaad6a4a491d589220cedfec2380d
makanai-j <kd1365575@st.kobedenshi.ac.jp>
2025-08-22 00:36:39 +0000

    eslint

---
+++ test-ts/eslint.config.js
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+      globals: {
+        window: "readonly",
+        document: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs["recommended-requiring-type-checking"].rules,
+      complexity: ["error", 5],
+    },
+  },
+];
--- test-ts/index.ts
+++ test-ts/index.ts
@@ -16,3 +16,29 @@ console.log(users);
 users.length = 0;
 
 console.log(users);
+
+const a = (v: number) => {
+  if (v > 100) {
+    console.log("> 100");
+  } else if (v > 90) {
+    console.log("> 90");
+  } else if (v > 80) {
+    console.log("> 80");
+  } else if (v > 70) {
+    console.log("> 70");
+  } else if (v > 60) {
+    console.log("> 60");
+  } else if (v > 50) {
+    console.log("> 50");
+  } else if (v > 40) {
+    console.log("> 40");
+  } else if (v > 30) {
+    console.log("> 30");
+  } else if (v > 20) {
+    console.log("> 20");
+  } else if (v > 10) {
+    console.log("> 10");
+  } else {
+    console.log("<= 10");
+  }
+};
